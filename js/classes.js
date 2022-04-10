a2e2642de9f467a62d0eaecf4a717e09c750df00
Patrick Anderson Carvalho Dos Santos <pandersomm@gmail.com>
2022-04-10 20:56:40 -0300

    Co-authored-by: Leonardo Lago Moreno <lelamo2002@users.noreply.github.com> Co-authored-by: Cauã Matheus <caua10000@gmail.com>

--- js/classes.js
+++ js/classes.js
@@ -1,19 +1,56 @@
 class Sprite {
-  width = 50
-  height = 150
-
-  constructor({ position}) {
-    this.position = position;
-
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 }
+  }) {
+    this.position = position
+    this.width = 50
+    this.height = 150
+    this.image = new Image()
+    this.image.src = imageSrc
+    this.scale = scale
+    this.framesMax = framesMax
+    this.framesCurrent = 0
+    this.framesElapsed = 0
+    this.framesHold = 5
+    this.offset = offset
   }
 
   draw() {
+    c.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax),
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    )
+  }
+
+  animateFrames() {
+    this.framesElapsed++
+
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++
+      } else {
+        this.framesCurrent = 0
+      }
+    }
   }
 
   update() {
-    this.draw();
+    this.draw()
+    this.animateFrames()
+  }
 }
-}
+
 class Fighter {
   width = 50
   height = 150
@@ -25,7 +62,21 @@ class Fighter {
   isAttacking = false;
   isAttackingInCooldown = false;
   
-  constructor({ position, velocity, offset }) {
+  constructor({
+    position,
+    velocity,
+    color = 'red',
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset,
+    sprites,
+  }) {
+    super({
+      imageSrc,
+      scale,
+      framesMax,
+    })
     this.position = position;
     this.velocity = velocity;
     this.offset = offset ?? 0;
@@ -38,7 +89,16 @@ class Fighter {
       width: 100,
       height: 50,
     }
+    this.framesCurrent = 0
+    this.framesElapsed = 0
+    this.framesHold = 5
+    this.sprites = sprites
     this.health = 100;
+
+    for (const sprite in this.sprites) {
+      sprites[sprite].image = new Image()
+      sprites[sprite].image.src = sprites[sprite].imageSrc
+    }
   }
 
   draw() {
@@ -71,6 +131,7 @@ class Fighter {
   
   attack() {
     if (!this.isAttackingInCooldown) {
+      this.switchSprite('attack1');
       this.isAttacking = true;
       this.isAttackingInCooldown = true;
       setTimeout(() => {
